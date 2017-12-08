f8a516a510bbdb03f76ee0911cbb9873454bb43e
Viktor Ayzenshtadt <ayzensht@uni-hildesheim.de>
2017-12-08 17:36:52 +0100

    Add mkdir command to executable scripts, remove unnecessary gitignores

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-javac -cp lib\jade.jar src\de\unihildesheim\iis\jadedemo\*.java && COPY src\de\unihildesheim\iis\jadedemo\*.class bin\de\unihildesheim\iis\jadedemo\ && java -cp lib\jade.jar;bin; de/unihildesheim/iis/jadedemo/ContainerController
+mkdir bin\de\unihildesheim\iis\jadedemo && javac -cp lib\jade.jar src\de\unihildesheim\iis\jadedemo\*.java && COPY src\de\unihildesheim\iis\jadedemo\*.class bin\de\unihildesheim\iis\jadedemo\ && java -cp lib\jade.jar;bin; de/unihildesheim/iis/jadedemo/ContainerController
